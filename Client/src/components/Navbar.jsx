ee139949195bcf48beabd299bb4a3bfe92a32353
AshutoshDM1 <downlodemaster1@gmail.com>
2024-06-13 19:55:54 +0530

    Integrated GSAP

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -1,27 +1,81 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useRef } from "react";
+import { motion } from "framer-motion";
+
 const NavBar = () => {
+  const Navs = useRef(null);
+  const Logos = useRef(null);
+
+  useGSAP(
+    () => {
+      const tl = gsap.timeline();
+      tl.from(Logos.current.children, {
+        x: -550,
+        stagger: 0.2,
+        duration: 1.2,
+      });
+      tl.from(Navs.current.children, {
+        y: -100,
+        stagger: 0.2,
+      });
+
+      return () => {
+        tl.kill();
+      };
+    },
+    { scope: Navs, Logos }
+  );
+
   return (
     <>
-      <div className=" text-white pr-10 pl-10  h-10h  text-gray justify-between flex items-center text-center gap-10">
-        <div className="h-10h flex items-center justify-between logo cursor-pointer">
-          <img className="mt-10 w-4vw object-cover" src="/wallet_logo.png" />
-          <h1 className="ml-5 mt-10 min-h-fit  font-bold text-2vw">
+      <div className="text-white pr-10 pl-10 h-10h text-gray justify-between flex items-center text-center gap-10">
+        <div
+          ref={Logos}
+          className="h-10h flex items-center justify-between logo cursor-pointer"
+        >
+          <img
+            className="mt-10 w-4vw object-cover"
+            src="/wallet_logo.png"
+            alt="Wallet Logo"
+          />
+          <h1 className="ml-5 mt-10 min-h-fit font-bold text-2vw">
             PrimeWallet
           </h1>
         </div>
-        <div className="navs flex items-center gap-10 justify-center font-medium ">
-          <h1 className="min-h-fit  text-xl cursor-pointer pt-2 pb-2 pl-4 pr-4 border-white hover:border-gray-300 border-2 rounded-20r ">
+        <div
+          ref={Navs}
+          className="navs flex items-center gap-10 justify-center font-medium "
+        >
+          <motion.h1
+            whileTap={{ scale: 1.1 }}
+            className="min-h-fit text-xl cursor-pointer pt-2 pb-2 pl-4 pr-4 border-white hover:border-gray-300 border-2 rounded-20r"
+          >
             GitHub
-          </h1>
-          <h1 className="min-h-fit  text-xl cursor-pointer pt-2 pb-2 pl-4 pr-4 border-white hover:border-gray-300 border-2 rounded-20r ">SignUp</h1>
-          <h1 className="min-h-fit  text-xl cursor-pointer pt-2 pb-2 pl-4 pr-4 border-white hover:border-gray-300 border-2 rounded-20r ">Login</h1>
-          <h1 className="flex justify-center items-center h-10 w-12vw  text-xl cursor-pointer text-white bg-purple-600 border-white rounded-20r  hover:bg-purple-500 ">
+          </motion.h1>
+          <motion.h1
+            whileTap={{ scale: 1.1 }}
+            className="min-h-fit text-xl cursor-pointer pt-2 pb-2 pl-4 pr-4 border-white hover:border-gray-300 border-2 rounded-20r"
+          >
+            SignUp
+          </motion.h1>
+          <motion.h1
+            whileTap={{ scale: 1.1 }}
+            className="min-h-fit text-xl cursor-pointer pt-2 pb-2 pl-4 pr-4 border-white hover:border-gray-300 border-2 rounded-20r"
+          >
+            Login
+          </motion.h1>
+          <motion.h1
+            whileTap={{ scale: 1.1 }}
+            className="flex justify-center items-center h-10 w-12vw text-xl cursor-pointer text-white bg-purple-600 border-white rounded-20r hover:bg-purple-500"
+          >
             Get-Started
-          </h1>
-          <h1 className="min-h-fit font-bold text-xl cursor-pointer "></h1>
+          </motion.h1>
+          <h1 className="min-h-fit font-bold text-xl cursor-pointer"></h1>
         </div>
       </div>
     </>
   );
-}; 
+};
 
 export default NavBar;
--- Client/src/page/Home.jsx
+++ Client/src/page/Home.jsx
@@ -1,18 +1,71 @@
 import NavBar from "../components/Navbar";
 import heroPic from "../assets/hero2_logo.png";
 import heroPhone from "../assets/hero_phone.png";
-function Home() {
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useRef } from "react";
+const Home = () => {
+  const Pic1 = useRef(null);
+  const Pic2 = useRef(null);
+  const headers = useRef(null);
+
+  useGSAP(
+    () => {
+      const tl = gsap.timeline({
+        defaults: { duration: 1, ease: "power2.out" },
+      });
+      const tl2 = gsap.timeline({
+        defaults: { duration: 1, ease: "power2.out" },
+      });
+      tl2.fromTo(
+        Pic1.current,
+        { opacity: 0, x: -450 },
+        { opacity: 1, x: 0,  }
+      );
+
+      tl.fromTo(
+        Pic2.current,
+        { opacity: 0, x: 150, scale: 0.8 },
+        { opacity: 1, x: -750, scale: 1 }
+      );
+      tl.fromTo(
+        headers.current.children,
+        {
+          x:-100,
+          scale: 3,
+          opacity: 0,
+        },
+        {
+          x:480,
+          stagger:.4,
+          scale:1,
+          duration:1.3,
+          opacity: 1,
+        }
+      );
+      return () => {
+        tl.kill();
+        tl2.kill();
+      };
+    },
+    { scope: Pic1, Pic2, headers }
+  );
+
   return (
     <>
       <div className="home h-90h w-90w rounded-20r  overflow-hidden">
         <NavBar />
         <div className="h-90h w-full flex">
           <div className="container h-full  w-1/3 flex items-center justify-center ">
-            <img src={heroPic} className="h-3/4" />
+            <img src={heroPic} ref={Pic1} className="h-3/4" />
           </div>
           <div className="ml-10 container h-full text-white flex flex-col gap-2 displa items-center justify-center w-1/2">
-            <img src={heroPhone} className="pic absolute right-32 -z-5 h-2/5" />
-            <div className="text absolute -ml-52 right-60h z-10">
+            <img
+              src={heroPhone}
+              ref={Pic2}
+              className="pic absolute right-32 -z-5 h-2/5"
+            />
+            <div ref={headers} className="text absolute -ml-52 right-60h z-10">
               <h1 className="font-bold text-2vw mt-10vw">
                 Instant Money Transfers,
               </h1>
@@ -26,6 +79,6 @@ function Home() {
       </div>
     </>
   );
-}
+};
 
 export default Home;
